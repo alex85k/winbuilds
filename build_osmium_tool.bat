67bd70c394fef399f872f0be5f6376aaae603156
alex85k <alex85k@github.com>
2014-08-28 18:02:27 +0600

    fix osmium-tool build

--- build_osmium_tool.bat
+++ build_osmium_tool.bat
@@ -1,13 +1,12 @@
 call settings.bat
 
-rem git clone https://github.com/osmcode/osmium-tool
-git clone https://github.com/alex85k/osmium-tool.git
+git clone https://github.com/osmcode/osmium-tool
 cd osmium-tool
 
 mkdir build
 cd build
 cmake .. -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -T CTP_Nov2013
-msbuild /clp:Verbosity=minimal /nologo osmium.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
-msbuild /clp:Verbosity=minimal /nologo INSTALL.vcxproj
+msbuild /p:Configuration=%Variant% /clp:Verbosity=minimal /nologo osmium.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
+msbuild /p:Configuration=%Variant% /clp:Verbosity=minimal /nologo INSTALL.vcxproj
 
 cd ..\..
