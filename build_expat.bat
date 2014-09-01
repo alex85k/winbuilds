caa689f0591cd930de5d81064b5669010e4a1dce
alex85k <alex85k@github.com>
2014-09-01 23:01:49 +0600

    use static expat library (hardcode XML_STATIC)

--- build_expat.bat
+++ build_expat.bat
@@ -1,8 +1,10 @@
 call settings.bat
 
-git clone https://github.com/alex85k/expat.git
+git clone https://github.com/coapp-packages/expat.git
 cd expat
-cmake -G %CMAKE_TARGET% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_shared=ON
 
+"%SED%" -i lib/expat_external.h -e "s@defined(XML_STATIC)@1@"
+
+cmake -G %CMAKE_TARGET% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_shared=OFF
 %MAKE% clean install
 cd ..
