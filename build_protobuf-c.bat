2c1d9a992186164fd6a5c3c4cb834731d99bad06
alex85k <alex85k@github.com>
2014-09-13 10:45:04 +0400

    use official repository for protobuf-c

--- build_protobuf-c.bat
+++ build_protobuf-c.bat
@@ -1,6 +1,6 @@
 call settings.bat
 
-git clone https://github.com/alex85k/protobuf-c.git 
+git clone https://github.com/protobuf-c/protobuf-c.git 
 cd protobuf-c/build-cmake
 cmake -G %CMAKE_TARGET% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX%
 %MAKE% install 
