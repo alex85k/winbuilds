83abce05805384ad582e5612e6c1f0440785a976
alex85k <alex85k@github.com>
2014-09-17 16:39:34 +0600

    fix iconv release install

--- build_iconv.bat
+++ build_iconv.bat
@@ -6,9 +6,9 @@ cd libiconv\COPKG\libiconv
 devenv  libiconv.sln /Upgrade /nologo
 msbuild /p:Configuration=%Variant% /clp:Verbosity=minimal /nologo libiconv.vcxproj /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
 
-copy /y x64\Debug\*.lib %PREFIX%\lib
-copy /y x64\Debug\libiconv.lib %PREFIX%\lib\iconv.lib
-copy /y x64\Debug\*.dll %PREFIX%\bin
+copy /y x64\%Variant%\*.lib %PREFIX%\lib
+copy /y x64\%Variant%\libiconv.lib %PREFIX%\lib\iconv.lib
+copy /y x64\%Variant%\*.dll %PREFIX%\bin
 copy /y *.h %PREFIX%\include
 
 cd ..\..\..
