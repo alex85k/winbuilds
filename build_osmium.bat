33d2dc17075e722295fd625d19ca742c5312a1ed
alex85k <alex85k@github.com>
2014-09-17 17:24:52 +0600

    fix libosmium release tests

--- build_osmium.bat
+++ build_osmium.bat
@@ -6,7 +6,7 @@ git clone https://github.com/osmcode/libosmium.git libosmium
 cd libosmium/build-cmake
 set TESTS=ON
 set ALLHPPS=OFF
-cmake .. -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBUILD_TESTING=%TESTS% -DBUILD_TRY_HPPS=%ALLHPPS% -T CTP_Nov2013
+cmake .. -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=%Variant% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBUILD_TESTING=%TESTS% -DBUILD_TRY_HPPS=%ALLHPPS% -T CTP_Nov2013
 msbuild /p:Configuration=%Variant% /clp:Verbosity=minimal /nologo libosmium.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
 
 set PATH=%PATH%;%PREFIX%/bin
