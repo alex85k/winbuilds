27d55384ffb68fe072c077753100220838ec47f6
alex85k <alex85k@github.com>
2014-09-22 19:29:35 +0400

    use only svn to get geos

--- build_geos.bat
+++ build_geos.bat
@@ -2,7 +2,7 @@ setlocal
 call settings.bat
 
 svn checkout http://svn.osgeo.org/geos/trunk geos-svn
-git svn clone -r HEAD http://svn.osgeo.org/geos/trunk geos-svn
+rem git svn clone -r HEAD http://svn.osgeo.org/geos/trunk geos-svn
 
 cd geos-svn
 echo #define GEOS_SVN_REVISION "3.4.2.svn" > geos_svn_revision.h
