578e85614cbb3961656b7a12b36db52b44526319
alex85k <alex85k@github.com>
2014-09-23 20:30:25 +0400

    fix spatialite-tools on MinGW

--- build_spatialite_tools.bat
+++ build_spatialite_tools.bat
@@ -3,14 +3,26 @@ call settings.bat
 
 call fetch.bat http://www.gaia-gis.it/gaia-sins/spatialite-tools-4.2.0.tar.gz spatialite-tools-4.2.0
 
-if "%COMPILER%" == "MINGW" (
-   echo "Sorry, MINGW is not supported for now"
-   exit /b 1
-)
-
 cd spatialite-tools-4.2.0
 
-"%SEDC%" -i nmake.opt -e s@INSTDIR=.*@INSTDIR=\$\(PREFIX\)@"
+if "%COMPILER%" == "MINGW" (
+
+  SET "PATH=%MSYSDIR%;%PREFIX%\bin;%PATH%"
+  set "LIBSPATIALITE_CFLAGS=%CFLAGS%"
+  set "LIBSPATIALITE_LIBS=%LDFLAGS%"
+  set "LIBFREEXL_CFLAGS=%CFLAGS%"
+  set "LIBFREEXL_LIBS=%LDFLAGS%"
+  set "LIBREADOSM_CFLAGS=%CFLAGS%"
+  set "LIBREADOSM_LIBS=%LDFLAGS%"
+
+  if NOT EXIST Makefile (bash -c "./configure --prefix=%PREFIX:\=/%")
+  %ER%
+  bash -c "make install"
+  %ER%
+
+) else (
+
+"%SEDC%" -i nmake.opt -e "s@INSTDIR=.*@INSTDIR=\$\(PREFIX\)@"
 if "%Variant%" == "Debug" (
     "%SEDC%" -i nmake.opt -e "s@\/MD @\/MDd \/Zi @"
     "%SEDC%" -i nmake.opt -e "s@\/Ox@@"
@@ -27,3 +39,4 @@ rem MAKE THE MOST LIBS STATICALLY LINKED TO AVOID DLL CONFLICTS
 
 nmake -f makefile.vc install
 %ER%
+)
