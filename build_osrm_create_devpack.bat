63b816bb6a4e86e638bbda320428f7d9d4298fd3
alex85k <alex85k@github.com>
2014-11-23 15:29:36 +0500

    add ruby packng to osrm devpack

--- build_osrm_create_devpack.bat
+++ build_osrm_create_devpack.bat
@@ -2,3 +2,9 @@ call settings.bat
 
 set PATH=C:\Program Files (x86)\Git\bin;%PATH%
 bash -c "bash ./build_osrm_create_devpack.sh"
+cd osrm_libs
+
+xcopy /y /e ..\..\ruby ruby\
+
+..\7z a ../libs_osrm_%Variant%.7z libs boost_min tbb *.bat -t7z -mx=9
+..\7z a ../libs_osrm_%Variant%_test.7z libs boost_min tbb ruby *.bat -t7z -mx=9
