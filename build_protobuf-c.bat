10ab691288a9158f795ba8b58ac3567505f4c51e
alex85k <alex85k@github.com>
2015-05-06 21:01:13 +0500

    use my repostitory for protobuf-c until the fix in main

--- build_protobuf-c.bat
+++ build_protobuf-c.bat
@@ -1,12 +1,14 @@
 setlocal
 call settings.bat
 
-git clone https://github.com/protobuf-c/protobuf-c.git 
+rem git clone -b master https://github.com/protobuf-c/protobuf-c.git
+git clone -b master https://github.com/alex85k/protobuf-c.git
 
 cd protobuf-c/build-cmake
 
 %CMAKE%
 %ER%
+%MAKEC% clean
 %MAKEC% install
 %ER%
 
