e640cebab52eaa517238fd2eded404aa9db3db44
alex85k <alex85k@github.com>
2015-05-07 16:33:43 +0500

    update readosm to 1.0.0d

--- build_readosm.bat
+++ build_readosm.bat
@@ -1,9 +1,9 @@
 setlocal
 call settings.bat
 
-call fetch.bat http://www.gaia-gis.it/gaia-sins/readosm-1.0.0b.tar.gz readosm-1.0.0b
+call fetch.bat http://www.gaia-gis.it/gaia-sins/readosm-1.0.0d.tar.gz readosm-1.0.0d
 
-cd readosm-1.0.0b
+cd readosm-1.0.0d
 
 if NOT EXIST %PREFIX%\include\expat.h ( echo "Sorry, need expat to build" & exit /b 1 )
 
