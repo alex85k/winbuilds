87ff24e07341818b2a31f4ff37141851381797b8
alex85k <alex85k@github.com>
2015-05-07 17:11:14 +0500

    swith geos to git, use parallel msbuild for it

--- build_geos.bat
+++ build_geos.bat
@@ -1,19 +1,24 @@
 setlocal
 call settings.bat
 
-svn checkout http://svn.osgeo.org/geos/trunk geos-svn
-rem git svn clone -r HEAD http://svn.osgeo.org/geos/trunk geos-svn
+rem svn checkout http://svn.osgeo.org/geos/trunk geos-svn
+git clone https://github.com/libgeos/libgeos.git geos
 
+cd geos
 
-cd geos-svn
 if "%COMPILER:MSVC=%"=="%COMPILER%" (
   svn patch ..\std_check.diff
   svn patch ..\mingw_geos_config.diff
   set "MOREOPTS=-j4"
 )
 
+"%SEDC%" -i CMakeLists.txt -e "s@OR MSVC13)@OR MSVC13 OR MSVC14)@"
 if NOT EXIST .svn ( echo #define GEOS_SVN_REVISION "3.4.2.svn" > geos_svn_revision.h )
-%CMAKE% -DGEOS_ENABLE_TESTS=OFF
-%MAKEC% install %MOREOPTS%
+
+SET PREFIX1=%PREFIX:\=/%
+cmake -G "%GENERATOR%" -DGEOS_ENABLE_TESTS=OFF -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1%
+msbuild /p:Configuration=%Variant% /p:BuildInParallel=true /clp:Verbosity=normal /nologo geos.sln
+msbuild /p:Configuration=%Variant% /p:BuildInParallel=true /clp:Verbosity=normal /nologo INSTALL.vcxproj
+
 cd ..
                                   
