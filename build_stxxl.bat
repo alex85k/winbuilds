4cfef8973f4d2f4d483543c0eaf450edcfd169a2
alex85k <alex85k@github.com>
2015-05-07 21:49:57 +0500

    temporary use my fork for stxxl

--- build_stxxl.bat
+++ build_stxxl.bat
@@ -2,17 +2,22 @@ setlocal
 call settings.bat
 
 rem git clone https://github.com/DennisOSRM/stxxl.git
-git clone https://github.com/stxxl/stxxl.git
+rem git clone https://github.com/stxxl/stxxl.git
+git clone https://github.com/alex85k/stxxl.git
 
 cd stxxl
 mkdir build
 cd build
 
 
-cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX%
+cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_TESTS=ON
 %ER%
 %MSBUILDINSTALL%
 %ER%
+
+if  "%Variant%" == "Debug"  (
+copy %PREFIX%\lib\stxxl_debug.lib %PREFIX%\lib\stxxl.lib
+)
 
 cd ..
 cd ..
