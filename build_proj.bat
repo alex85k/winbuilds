4ebabf656de913709e2f2e9eb31373569804c075
alex85k <alex85k@github.com>
2015-08-17 21:03:45 +0500

    Switch Proj.4 to cmake

--- build_proj.bat
+++ build_proj.bat
@@ -3,7 +3,6 @@ call settings.bat
 
 call fetch.bat http://download.osgeo.org/proj/proj-4.9.1.tar.gz proj-4.9.1
 cd proj-4.9.1
-
 set INSTDIR=%PREFIX:\=\\%
 if "%compiler%" == "MINGW" (
   SET "PATH=%MSYSDIR%;%PATH%"
@@ -12,16 +11,16 @@ if "%compiler%" == "MINGW" (
   bash -c "make install"
   %ER%
 ) else (
-  if "%Variant%" == "Debug" (
-     set OPTFLAGS=	/nologo /Zi /MDd /Fdproj.pdb
-  ) else (
-     set OPTFLAGS=	/nologo /Ox /Op /MD
-  )
-  set PROJ_LIB_DIR=%INSTDIR%\\share
-  nmake /f makefile.vc install-all
+  cmake %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -DPROJ_INCLUDE_SUBDIR=include -DPROJ_LIB_SUBDIR=lib -DBUILD_LIBPROJ_SHARED=YES
   %ER%
+  %MSBUILDINSTALL%
+  %ER%
+  copy /y %PREFIX%\lib\proj_4_9.lib %PREFIX%\lib\proj_i.lib 
+  cmake %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -DPROJ_INCLUDE_SUBDIR=include -DPROJ_LIB_SUBDIR=lib -DBUILD_LIBPROJ_SHARED=NO
+  %ER%
+  %MSBUILDINSTALL%
+  %ER%
+  copy /y %PREFIX%\lib\proj_4_9.lib %PREFIX%\lib\proj.lib 
 )
-
-copy /y src\*.pdb %PREFIX%\bin
 
 cd ..
