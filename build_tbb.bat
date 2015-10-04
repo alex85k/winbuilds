5154c75d9afa0b71f5cc152d598a08ffff509471
alex85k <alex85k@github.com>
2015-10-04 16:42:23 +0500

    building TBB

--- build_tbb.bat
+++ build_tbb.bat
@@ -1,6 +1,6 @@
 call settings.bat
 
-call fetch.bat https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb43_20140724oss_src.tgz tbb43_20140724oss
+call fetch.bat https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb44_20150728oss_src.tgz tbb44_20150728oss
 
 
 if "%COMPILER%" == "MINGW" (
@@ -10,16 +10,26 @@ cd tbb43_20140724oss
   %ER%
   cd ..
 ) else (
-cd tbb43_20140724oss\build\vs2010
-devenv  makefile.sln /Upgrade /nologo
-msbuild /p:Configuration=Debug /clp:Verbosity=minimal /nologo makefile.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
-msbuild /p:Configuration=Release /clp:Verbosity=minimal /nologo makefile.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
+cd tbb44_20150728oss\build\vs2010
+rem devenv  makefile.sln /Upgrade /nologo
+rem msbuild /p:Configuration=Debug /clp:Verbosity=minimal /nologo makefile.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
+rem msbuild /p:Configuration=Release /clp:Verbosity=minimal /nologo makefile.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
+echo Creating dirs
+mkdir %PREFIX%\tbb
+mkdir %PREFIX%\tbb\lib
+mkdir %PREFIX%\tbb\bin
+mkdir %PREFIX%\tbb\include
+mkdir %PREFIX%\tbb\lib\intel64
+mkdir %PREFIX%\tbb\bin\intel64
+mkdir %PREFIX%\tbb\lib\intel64\vc14
+mkdir %PREFIX%\tbb\bin\intel64\vc14
 
-copy /y intel64\Debug\*.lib %PREFIX%\lib
-copy /y intel64\Debug\*.dll %PREFIX%\bin
-copy /y intel64\Release\*.lib %PREFIX%\lib
-copy /y intel64\Release\*.dll %PREFIX%\bin
-xcopy /d /y /e ..\..\src\include %PREFIX%\include
+echo Copy files
+copy /y intel64\Debug\*.lib %PREFIX%\tbb\lib\intel64\vc14
+copy /y intel64\Debug\*.dll %PREFIX%\tbb\bin\intel64\vc14
+copy /y intel64\Release\*.lib %PREFIX%\tbb\lib\intel64\vc14
+copy /y intel64\Release\*.dll %PREFIX%\tbb\bin\intel64\vc14
+xcopy /d /y /e ..\..\src\include %PREFIX%\tbb\include
 
 cd ..\..\..
 )
