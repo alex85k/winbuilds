c0ebea0fb430638b220056f3b1843be3ae3a6ebc
alex85k <alex85k@github.com>
2015-10-10 22:45:54 +0500

    osrm script fix

--- build_osrm.bat
+++ build_osrm.bat
@@ -1,8 +1,8 @@
 setlocal
 call settings.bat
 
-set TBB_INSTALL_DIR=d:/tbb
-set TBB_ARCH_PLATFORM=intel64/vc12
+set TBB_INSTALL_DIR=%PREFIX%\tbb
+set TBB_ARCH_PLATFORM=intel64/vc14
  
 git clone -b develop https://github.com/Project-OSRM/osrm-backend osrm-backend
  
@@ -15,14 +15,13 @@ SET PREFIX1=%PREFIX:\=/%
 rem cmake .. -G %CMAKE_TARGET% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON
 rem use for debug:
 
+cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBoost_ADDITIONAL_VERSIONS=1.58;1.59
+msbuild /p:Configuration=%Variant% /clp:Verbosity=normal /nologo OSRM.sln
+
 if  "%Variant%" == "Debug"  (
-   cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON
-   msbuild /p:Configuration=%Variant% /clp:Verbosity=normal /nologo OSRM.sln
    copy /y Debug\*.pdb .
    copy /y Debug\*.exe .
 ) else (
-   cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON
-   msbuild /p:Configuration=%Variant% /clp:Verbosity=normal /nologo OSRM.sln
    copy /y Release\*.exe .
 )
 
