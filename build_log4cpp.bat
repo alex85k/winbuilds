bf8ff2d92338090a648d01a3badcbd7472eaaefd
alex85k <alex85k@github.com>
2015-10-13 01:01:51 +0500

    add log4cpp

---
+++ build_log4cpp.bat
@@ -0,0 +1,19 @@
+setlocal
+call settings.bat
+
+rem svn checkout http://svn.osgeo.org/geos/trunk geos-svn
+rem git clone --depth 1 git://git.sv.nongnu.org/freetype/freetype2.git
+call fetch.bat http://sourceforge.net/projects/log4cpp/files/log4cpp-1.1.2rc1.tar.gz log4cpp
+
+cd log4cpp
+
+mkdir build
+cd build
+
+cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1%
+%ER%
+%MSBUILDINSTALL%
+%ER%
+
+cd ..\..
+                                  
