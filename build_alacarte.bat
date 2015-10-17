3133edecb758b50f7cf0741ae66aea1cddd51465
alex85k <alex85k@github.com>
2015-10-18 00:32:45 +0500

    alacarte update

--- build_alacarte.bat
+++ build_alacarte.bat
@@ -2,16 +2,16 @@ setlocal
 call settings.bat
 
 rem svn checkout http://svn.osgeo.org/geos/trunk geos-svn
-git clone --depth 1 https://github.com/alex85k/alacarte.git
+git clone  https://github.com/alacarte-maps/alacarte.git
 
 cd alacarte
+git remote add my https://github.com/alex85k/alacarte.git
+
 
 mkdir build
 cd build
 
-cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBoost_ADDITIONAL_VERSIONS=1.58;1.59
-%ER%
-%MSBUILDINSTALL%
+cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBoost_ADDITIONAL_VERSIONS=1.58;1.59 -DBUILD_TESTS=ON
 %ER%
 %MSBUILD% tests.vcxproj
 %ER%
