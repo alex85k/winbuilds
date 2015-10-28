3c18bfe67cbcefb68420b4ff8f4d28b8cc06e4ed
alex85k <alex85k@github.com>
2015-10-28 14:46:12 +0500

    osm2pgsql from main repo

--- build_osm2pgsql.bat
+++ build_osm2pgsql.bat
@@ -5,7 +5,7 @@ set "PSQL_ROOT=C:\Program Files\PostgreSQL\9.4"
 set PGUSER=postgres
 set PGPASSWORD=admin
 
-git clone -b cmake --depth 10 https://github.com/alex85k/osm2pgsql.git
+git clone --depth 10 https://github.com/openstreetmap/osm2pgsql.git
 
 cd osm2pgsql
 mkdir build
@@ -13,9 +13,13 @@ cd build
 
 set CMAKE_PREFIX_PATH=%PREFIX%;C:\Program Files\PostgreSQL\9.4
 
-cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBUILD_TESTS=ON -DBoost_ADDITIONAL_VERSIONS=1.57;1.58;1.59
+cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBUILD_TESTS=ON
 %ER%
+rem nmake test-expire-tiles
+rem nmake test ARGS="--build-and-test"
+rem nmake check
 nmake install
+rem install
 %ER%
 
 cd ..\..
