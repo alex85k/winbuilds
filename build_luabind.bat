fd8eb22a6786d01d37890224e9a266b7f824d520
alex85k <alex85k@github.com>
2016-04-01 18:53:35 +0500

    enable boost 1.60 detection

--- build_luabind.bat
+++ build_luabind.bat
@@ -1,12 +1,12 @@
 setlocal
 call settings.bat
 
-rem git clone https://github.com/Oberon00/luabind.git
-git clone https://github.com/DennisOSRM/luabind
+git clone https://github.com/Oberon00/luabind.git
+rem git clone https://github.com/DennisOSRM/luabind
 
 cd luabind
 
-cmake %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBoost_ADDITIONAL_VERSIONS=1.58;1.59
+cmake %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBoost_ADDITIONAL_VERSIONS=1.58;1.59;1.60
 %ER%
 %MSBUILDINSTALL%
 %ER%
