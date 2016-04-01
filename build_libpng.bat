05c7a332758c7f13a1c21d11e79af7c76a7fba38
alex85k <alex85k@github.com>
2016-04-01 10:36:22 +0500

    libpng 1.6.21

--- build_libpng.bat
+++ build_libpng.bat
@@ -1,9 +1,9 @@
 setlocal
 call settings.bat
 
-call fetch.bat http://prdownloads.sourceforge.net/libpng/lpng1612.zip lpng1612
+call fetch.bat http://prdownloads.sourceforge.net/libpng/lpng1621.zip lpng1621
 
-cd lpng1612
+cd lpng1621
 %CMAKE%
 %ER%
 %MAKEC% install
