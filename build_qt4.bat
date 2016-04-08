22e37c8e4dd1ff0aa00cfa6755e24765b1d22bd9
alex85k <alex85k@github.com>
2016-04-08 18:16:34 +0500

    add qt4 (unfinished)

---
+++ build_qt4.bat
@@ -0,0 +1,17 @@
+setlocal
+call settings.bat
+
+call fetch.bat http://files.linux-addicted.net/qtproject/official_releases/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.zip qt-everywhere-opensource-src-4.8.7
+cd qt-everywhere-opensource-src-4.8.7
+
+
+configure -debug-and-release -shared  -prefix %PREFIX1%
+%ER%
+
+nmake
+%ER%
+
+nmake install
+%ER%
+
+cd ..
