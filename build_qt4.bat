a38e3154093ce674d3e1fdeb5bb4b1c59770f996
alex85k <alex85k@github.com>
2016-04-09 17:01:08 +0500

    fixing qt4

--- build_qt4.bat
+++ build_qt4.bat
@@ -2,10 +2,19 @@ setlocal
 call settings.bat
 
 call fetch.bat http://files.linux-addicted.net/qtproject/official_releases/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.zip qt-everywhere-opensource-src-4.8.7
+
+copy 02-fix_build_with_msvc2015-45e8f4ee.diff qt-everywhere-opensource-src-4.8.7
+
 cd qt-everywhere-opensource-src-4.8.7
+git init
+
+git apply 02-fix_build_with_msvc2015-45e8f4ee.diff
+
+del bin\syncqt.bat
+del bin\syncqt
 
 
-configure -debug-and-release -shared -no-qt3support -prefix %PREFIX1%
+echo o y | configure -debug-and-release -shared -no-qt3support -prefix %PREFIX%
 %ER%
 
 nmake
