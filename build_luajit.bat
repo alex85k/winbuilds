18f2ad4662f30e253485daa0a8896020f52cb0ed
alex85k <alex85k@github.com>
2017-04-02 20:09:18 +0500

    luajit (official git)

--- build_luajit.bat
+++ build_luajit.bat
@@ -1,14 +1,17 @@
 setlocal
 call settings.bat
 
-git clone https://github.com/LuaDist/luajit
+rem git clone https://github.com/LuaDist/luajit
+git clone --depth 10 -b v2.1 https://github.com/LuaJIT/LuaJIT.git
 
 cd luajit
 
-%CMAKE%
-%ER%
-%MAKEC% install
-%ER%
+cd src
+call msvcbuild.bat
 
+copy /y *.lib %PREFIX%\lib\
+copy /y lua*.h %PREFIX%\include\
+copy /y lua*.hpp %PREFIX%\include\
+copy /y laux*.h %PREFIX%\include\
 cd ..
 
