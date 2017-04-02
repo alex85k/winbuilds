7dba8baef995b96e305e5072208fe9ccf9dd9bcc
alex85k <alex85k@github.com>
2017-04-02 20:08:13 +0500

    tbb 2017-u5

--- build_tbb.bat
+++ build_tbb.bat
@@ -1,19 +1,20 @@
 call settings.bat
 
-call fetch.bat https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb44_20150728oss_src.tgz tbb44_20150728oss
+call fetch.bat https://github.com/01org/tbb/archive/2017_U5.zip tbb-2017_U5
+rem https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb44_20150728oss_src.tgz tbb44_20150728oss
 
 
 if "%COMPILER%" == "MINGW" (
-cd tbb43_20140724oss
+cd tbb-2017_U5
   SET "PATH=%MSYSDIR%;%PATH%"
   bash -c "make mingw"  
   %ER%
   cd ..
 ) else (
-cd tbb44_20150728oss\build\vs2010
-rem devenv  makefile.sln /Upgrade /nologo
+cd tbb-2017_U5\build\vs2012
+devenv  makefile.sln /Upgrade /nologo
 rem msbuild /p:Configuration=Debug /clp:Verbosity=minimal /nologo makefile.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
-rem msbuild /p:Configuration=Release /clp:Verbosity=minimal /nologo makefile.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
+msbuild /p:Configuration=Release /clp:Verbosity=minimal /nologo makefile.sln /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
 echo Creating dirs
 mkdir %PREFIX%\tbb
 mkdir %PREFIX%\tbb\lib
@@ -25,11 +26,11 @@ mkdir %PREFIX%\tbb\lib\intel64\vc14
 mkdir %PREFIX%\tbb\bin\intel64\vc14
 
 echo Copy files
-copy /y intel64\Debug\*.lib %PREFIX%\tbb\lib\intel64\vc14
-copy /y intel64\Debug\*.dll %PREFIX%\tbb\bin\intel64\vc14
-copy /y intel64\Release\*.lib %PREFIX%\tbb\lib\intel64\vc14
-copy /y intel64\Release\*.dll %PREFIX%\tbb\bin\intel64\vc14
-xcopy /d /y /e ..\..\src\include %PREFIX%\tbb\include
+copy /y x64\Debug\*.lib %PREFIX%\tbb\lib\intel64\vc14
+copy /y x64\Debug\*.dll %PREFIX%\tbb\bin\intel64\vc14
+copy /y x64\Release\*.lib %PREFIX%\tbb\lib\intel64\vc14
+copy /y x64\Release\*.dll %PREFIX%\tbb\bin\intel64\vc14
+xcopy /d /y /e ..\..\include %PREFIX%\tbb\include
 
 cd ..\..\..
 )
