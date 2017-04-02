28d6cb6faf05bd94f59ca5e27b4205dd57d9df6d
alex85k <alex85k@github.com>
2017-04-02 20:09:38 +0500

    stxxl

--- build_stxxl.bat
+++ build_stxxl.bat
@@ -2,16 +2,17 @@ setlocal
 call settings.bat
 
 rem git clone https://github.com/DennisOSRM/stxxl.git
-rem git clone https://github.com/stxxl/stxxl.git
-git clone --depth 10 https://github.com/thk1/stxxl.git
+git clone https://github.com/stxxl/stxxl.git
+rem git clone --depth 10 https://github.com/thk1/stxxl.git
 rem git clone https://github.com/alex85k/stxxl.git
 
 cd stxxl
 mkdir build
 cd build
 
-
-cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_TESTS=ON
+set CXXFLAGS=/bigobj
+rem cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_TESTS=ON
+cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF
 %ER%
 %MSBUILDINSTALL%
 %ER%
