17da547e991356f44de6176bf2a2ce63f92df504
alex85k <alex85k@github.com>
2017-04-14 18:09:49 +0500

    geos 3.6.1

--- build_geos.bat
+++ build_geos.bat
@@ -2,7 +2,7 @@ setlocal
 call settings.bat
 
 rem svn checkout http://svn.osgeo.org/geos/trunk geos-svn
-git clone --depth 1 -b 3.5.0 https://github.com/libgeos/libgeos.git geos
+git clone --depth 1 -b 3.6.1 https://github.com/libgeos/libgeos.git geos
 
 cd geos
 
@@ -12,7 +12,7 @@ if "%COMPILER:MSVC=%"=="%COMPILER%" (
   set "MOREOPTS=-j4"
 )
 
-"%SEDC%" -i CMakeLists.txt -e "s@OR MSVC13)@OR MSVC13 OR MSVC14)@"
+rem "%SEDC%" -i CMakeLists.txt -e "s@OR MSVC13)@OR MSVC13 OR MSVC14)@"
 if NOT EXIST .svn ( echo #define GEOS_SVN_REVISION "3.5.0" > geos_svn_revision.h )
 
 cmake %GENERATOR% -DGEOS_ENABLE_TESTS=OFF -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1%
