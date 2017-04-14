8e977e043e203deb9b05090269aafd6dbb302055
alex85k <alex85k@github.com>
2017-04-14 18:11:47 +0500

    osmium fixes

--- build_osmium_contrib.bat
+++ build_osmium_contrib.bat
@@ -13,9 +13,9 @@ if "%COMPILER%" == "MINGW" (
    mingw32-make
    %ER%
 ) else (
-   cmake .. %GENERATOR% -DCMAKE_BUILD_TYPE=%Variant% -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON -DBoost_ADDITIONAL_VERSIONS=1.58;1.59;1.60
+   %CMAKE% .. %CMAKE_BOOST_FLAGS% -DOsmium_DEBUG=ON
    %ER%
-   %MSBUILD% osmium-contrib.sln
+   %MAKEC%
    %ER%
 )
 set PATH=%PREFIX%/bin;%PATH%
