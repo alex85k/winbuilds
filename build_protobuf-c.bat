62c41fb3f05423eefce2518d45eaccef460a7120
alex85k <alex85k@github.com>
2017-04-14 18:11:17 +0500

    protobuf-c

--- build_protobuf-c.bat
+++ build_protobuf-c.bat
@@ -1,14 +1,15 @@
 setlocal
 call settings.bat
 
-git clone -b master https://github.com/protobuf-c/protobuf-c.git
+git clone -b next https://github.com/protobuf-c/protobuf-c.git
+rem git clone -b next https://github.com/alex85k/protobuf-c.git
 
 cd protobuf-c/build-cmake
 
 %CMAKE%
 %ER%
 %MAKEC% clean
-%MAKEC% install
+%MAKEC% VERBOSE=1 install
 %ER%
 
 if "%Variant%" == "Debug" (
