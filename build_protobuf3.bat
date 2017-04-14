e49c3402e7d98f5860180c3a85f21a57bef630a6
alex85k <alex85k@github.com>
2017-04-14 18:10:46 +0500

    protobuf-debug lib names fix

--- build_protobuf3.bat
+++ build_protobuf3.bat
@@ -8,10 +8,15 @@ git clone -b v3.2.0 --depth 10 https://github.com/google/protobuf.git
 cd protobuf/cmake
 
 cmake %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX1% -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_EXAMPLES=ON -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_WITH_ZLIB=ON
-%MSBUILDINSTALL%
+%MSBUILD%
 %ER%
 %MSBUILDINSTALL%
 %ER%
 
-rem ctest -VV 1>tests.log 2>&1
+echo Copying *d.lib files to *.lib, or CMake may not find it
+copy /y %PREFIX%\lib\libprotocd.lib %PREFIX%\lib\libprotoc.lib 
+copy /y %PREFIX%\lib\libprotobufd.lib %PREFIX%\lib\libprotobuf.lib 
+copy /y %PREFIX%\lib\libprotobuf-lited.lib %PREFIX%\lib\libprotobuf-lite.lib
+
+ctest -VV 1>tests.log 2>&1
 cd ../..
