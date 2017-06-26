30615e884cfb7d437918eec77eda8a7f47a912fd
alex85k <alex85k@github.com>
2017-06-26 22:18:39 +0500

    remove protobuf and luabind building for OSRM

--- build_all_osrm.bat
+++ build_all_osrm.bat
@@ -4,10 +4,8 @@ set "ERRORS="
 && call wlog.bat build_expat.bat  ^
 && call wlog.bat build_luajit.bat ^
 && call wlog.bat build_getopt.bat ^
-&& call wlog.bat build_protobuf.bat ^
 && call wlog.bat build_osmium.bat ^
 && call wlog.bat build_stxxl.bat ^
-&& call wlog.bat build_luabind.bat ^
 && call wlog.bat build_osrm.bat
 
 echo Builds with errors: %ERRORS%
