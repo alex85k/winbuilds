4bc6f52bc05e5124bd60e87c5532aaeaeae03735
alex85k <alex85k@github.com>
2017-09-11 00:15:43 +0500

    Support VS2017 Community

--- settings.bat
+++ settings.bat
@@ -1,9 +1,11 @@
-if "%COMPILER%"=="" SET COMPILER=MSVC2015
-if "%PREFIX%"=="" SET PREFIX=d:\libs2015r
+if "%COMPILER%"=="" SET COMPILER=MSVC2017
+if "%PREFIX%"=="" SET PREFIX=d:\libs2017r
 if "%BOOST_ROOT%"=="" SET BOOST_ROOT=d:/boost
 if "%VARIANT%"=="" SET Variant=Release
-
 set "ER=if ERRORLEVEL 1 exit /b 1"
+
+set "OLDDIR=%CD%"
+
 if "%COMPILER%"=="MINGW" (
   SET "PATH=%PATH%;c:\mingw64\bin"
   SET "MSYSDIR=c:\msys64\usr\bin"
@@ -49,13 +51,19 @@ if "%COMPILER%"=="MINGW" (
      set "GENERATOR= -G"Visual Studio 14 2015 Win64""
      call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
   )
+  if "%COMPILER%"=="MSVC2017"  (
+     set MSC_VER=15
+     set "GENERATOR= -G"Visual Studio 15 2017 Win64""
+     call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+     cd /d "%OLDDIR%"
+  )
   set "CMAKE=cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX%"
   SET "MAKEC=nmake"
   SET PREFIX1=%PREFIX:\=/%
   SET "MSBUILDINSTALL=msbuild /p:Configuration=%Variant% /p:BuildInParallel=true /clp:Verbosity=normal /nologo  /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly INSTALL.vcxproj"
   SET "MSBUILD=msbuild /p:Configuration=%Variant% /p:BuildInParallel=true /clp:Verbosity=normal /nologo  /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly"
   SET "SEDC=C:\Program Files\Git\usr\bin\sed"
-  SET "CMAKE_BOOST_FLAGS=-DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON  -DBoost_ADDITIONAL_VERSIONS=1.58;1.59;1.60;1.61;1.62;1.63;1.64"
+  SET "CMAKE_BOOST_FLAGS=-DBOOST_ROOT=%BOOST_ROOT% -DBoost_USE_STATIC_LIBS=ON  -DBoost_ADDITIONAL_VERSIONS=1.58;1.59;1.60;1.61;1.62;1.63;1.64;1.65;1.65.1"
 )
 set "CMAKEVS=cmake %GENERATOR% -DCMAKE_BUILD_TYPE=%VARIANT% -DCMAKE_INSTALL_PREFIX=%PREFIX%"
 
